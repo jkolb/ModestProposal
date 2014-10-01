add8b3bd023ef8e5af7ae1ddac4229b420c0fc9f
jkolb <franticapparatus@gmail.com>
2014-10-01 16:22:20 -0500

    Initial Commit

---
+++ ModestProposal/ModestProposal.h
@@ -0,0 +1,19 @@
+//
+//  ModestProposal.h
+//  ModestProposal
+//
+//  Created by Justin Kolb on 10/1/14.
+//  Copyright (c) 2014 Justin Kolb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModestProposal.
+FOUNDATION_EXPORT double ModestProposalVersionNumber;
+
+//! Project version string for ModestProposal.
+FOUNDATION_EXPORT const unsigned char ModestProposalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModestProposal/PublicHeader.h>
+
+
